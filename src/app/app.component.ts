79058028d49bac537f835819cc8499729b46512a
Kelly <jessicakelly352@gmail.com>
2021-05-10 22:39:27 -0300

    ajustes finais

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,49 +7,31 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  title = "filtroDigitacao";
   public cadastro: FormGroup;
+  emailRec: string = "";
+  senhaRec: string = "";
 
   constructor(private fb: FormBuilder) {
     this.cadastro = this.fb.group({ email: [""], senha: [""] });
   }
 
-  ngOnInit() {}
-  validaEmail(emailValue: any) {
-    let email = emailValue.target.value;
-    console.log(email);
+  ngOnInit() {
+    this.criarForm();
+  }
+  criarForm() {
     this.cadastro = this.fb.group({
       email: [
-        email,
+        "",
         Validators.compose([
           Validators.required,
           Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
         ])
-      ]
-    });
-    console.log("email");
-    console.log(this.cadastro.get("email")?.valid);
-    console.log(this.cadastro.get("email")?.value);
-    // this.ngOnInit();
-  }
-  validaSenha(senhaValue: any) {
-    let senha = senhaValue.target.value;
-    console.log(senha);
-    this.cadastro = this.fb.group({
+      ],
       senha: [
-        senha,
-        Validators.compose([
-          Validators.required,
-          Validators.pattern(
-            "^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{8,}$"
-          )
-        ])
+        "",
+        Validators.compose([Validators.required, Validators.minLength(6)])
       ]
     });
-    console.log("senha");
-    console.log(this.cadastro.get("senha")?.valid);
-    console.log(this.cadastro.get("senha")?.value);
-    // this.ngOnInit();
   }
   get email() {
     return this.cadastro.get("email");
